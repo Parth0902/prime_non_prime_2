5ebb0cc1aed31423cb30135899acb4ec3e023ba1
Parth0902 <102846775+Parth0902@users.noreply.github.com>
2023-05-10 19:55:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 num=input("Enter the number")
 
-# To take input from the user
-#num = int(input("Enter a number: "))
 
 if num == 1:
     print(num, "is not a prime number")
 elif num > 1:
-   # check for factors
+
    for i in range(2,num):
        if (num % i) == 0:
            print(num,"is not a prime number")
@@ -15,7 +13,6 @@ elif num > 1:
    else:
        print(num,"is a prime number")
        
-# if input number is less than
-# or equal to 1, it is not prime
+
 else:
    print(num,"is not a prime number")
